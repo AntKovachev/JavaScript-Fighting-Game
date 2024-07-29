a4c00ab4401dbc5f9e218376e5c94468c216595a
Anton Kovachev <ant.kovachev@gmail.com>
2024-07-29 23:14:18 +0300

    Add idle player animation
    Fixes #17

--- script.js
+++ script.js
@@ -40,6 +40,13 @@ const player = new Fighter({
     offset: {
         x: 0,
         y: 0
+    },
+    imageSrc: './assets/samuraiMack/Idle.png',
+    framesMax: 8,
+    scale: 2.5,
+    offset: {
+        x: 215,
+        y: 157
     }
 });
 
@@ -90,7 +97,7 @@ function animate() {
     background.update();
     shop.update();
     player.update();
-    enemy.update();
+    // enemy.update();
 
     //Player is not moving
     player.velocity.x = 0;
