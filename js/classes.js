487e75c499f0daa7c858707b216edec7bd50ab8b
Anton Kovachev <ant.kovachev@gmail.com>
2024-07-31 12:36:13 +0300

    Add attack animation for player
    Fixes #24

--- js/classes.js
+++ js/classes.js
@@ -116,6 +116,7 @@ class Fighter extends Sprite
     }
 
     attack() {
+        this.switchSprite('attack1');
         this.isAttacking = true;
         setTimeout(() => {
             this.isAttacking = false;
@@ -124,6 +125,10 @@ class Fighter extends Sprite
 
     //Responsible for switching between different sprites
     switchSprite(sprite) {
+        //This gets triggered when attacking
+        if (this.image === this.sprites.attack1.image && this.framesCurrent < this.sprites.attack1.framesMax - 1) {
+            return;
+        }
         switch (sprite) {
             case 'idle':
                 if (this.image !== this.sprites.idle.image) {
@@ -153,6 +158,14 @@ class Fighter extends Sprite
                 if (this.image !== this.sprites.fall.image) {
                     this.image = this.sprites.fall.image;
                     this.framesMax = this.sprites.fall.framesMax;
+                    this.framesCurrent = 0;
+                }
+                break;
+                
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image) {
+                    this.image = this.sprites.attack1.image;
+                    this.framesMax = this.sprites.attack1.framesMax;
                     this.framesCurrent = 0;
                 }
                 break;
